c16e3c815b3a268d42a6173e18eb145dd4ac7db6
Shuklaaa <shuklaabhishek0404@gmail.com>
2023-05-19 13:19:13 +0530

    initial commit with creating page routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import HomePage from './Pages/HomePage';
+import CoinPage from './Pages/CoinPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    
+      <div>
+
+        <Header/>
+
+        <Routes>
+        <Route exact path='/' element={<HomePage/>}/>
+        <Route exact path='/coins/:id' element={<CoinPage/>}/>
+        </Routes>
+      </div>
+      
+    </BrowserRouter>
   );
 }
 
